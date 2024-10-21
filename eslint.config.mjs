23e6bfbaa4636e54ae978acc4a53172360ef5e85
idris_babay_cgi <idrisbabay.cgi@gmail.com>
2024-10-21 02:05:51 +0200

    Change the order of the flat eslint configs to obey config overriding mechanism, add react version. Research and inform myself more on ESLint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,28 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
+// configs override each other by order, from top to the bottom of the array
+// Configs (incl. extended from pluggings) may be extracted/displayed via:
+// `./node_modules/.bin/eslint --print-config eslint.config.mjs > "./.eslintrc.js_fullsettings.js"`
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
-  {rules: {
-    semi: ["error", "always"],
-      "prefer-const": "error"
-  }
-  },
-  {ignores: ["node_modules", "dist"]},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules: {
+      semi: "error",
+      "prefer-const": "error",
+    },
+  },
+  {ignores: ["node_modules/**/*.{js,mjs,cjs,ts,jsx,tsx}",
+      "dist/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
 ];
